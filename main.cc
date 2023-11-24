3f5200ac7b9e799cbecac1d62c88243a392f302a
Arun Nanduri <arun.nanduri@gmail.com>
2023-11-24 11:59:50 -0500

    Format code.

--- main.cc
+++ main.cc
@@ -1,6 +1,6 @@
 #include <algorithm>
-#include <cmath>
 #include <chrono>
+#include <cmath>
 //#include <format>
 #include <fstream>
 #include <iostream>
@@ -15,7 +15,6 @@ std::random_device rd;
 std::mt19937 gen(rd());
 static constexpr double PI = 3.141592653;
 static constexpr double TIMESTEP = 0.1;
-
 
 std::vector<V2> genInitialPositions() {
   // Fictional coordinate system has (0, 0) at the bottom left,
@@ -40,12 +39,13 @@ double pix2pic(const int i, const int width) {
   return static_cast<double>(i) / width;
 }
 
-void step_particles(std::vector<Particle*>& particles) {
+void step_particles(std::vector<Particle *> &particles) {
   // Have timestep controlled via CLI flags (number & spacing)
-  for (Particle* p : particles) {
+  for (Particle *p : particles) {
     double const x_from_center = p->position.x - 0.5;
     double const y_from_center = p->position.y - 0.5;
-    double const radial_dist = sqrt(x_from_center * x_from_center + y_from_center * y_from_center);
+    double const radial_dist =
+        sqrt(x_from_center * x_from_center + y_from_center * y_from_center);
     double const new_radial_dist = radial_dist + p->speed_r * TIMESTEP;
     p->position.x = (new_radial_dist / radial_dist) * x_from_center + 0.5;
     p->position.y = (new_radial_dist / radial_dist) * y_from_center + 0.5;
@@ -53,18 +53,22 @@ void step_particles(std::vector<Particle*>& particles) {
 }
 
 // Can add parellelism here.
-void render_particles(std::vector<Particle*> const& particles, QuadTree const& quadtree, int const width, int const height, int const index, bool const use_quadtree) {
+void render_particles(std::vector<Particle *> const &particles,
+                      QuadTree const &quadtree, int const width,
+                      int const height, int const index,
+                      bool const use_quadtree) {
   std::ofstream file_handle;
-  //file_handle.open(std::format("output/img_{}", index));
-  // Do I have to use the abolute path?
-  file_handle.open("/Users/drake/Documents/projects/genart/fire_cpp/output/img_0.ppm");
+  // file_handle.open(std::format("output/img_{}", index));
+  //  Do I have to use the abolute path?
+  file_handle.open(
+      "/Users/drake/Documents/projects/genart/fire_cpp/output/img_0.ppm");
   if (!file_handle.is_open()) {
     std::cout << "file not open\n";
     return;
   }
   file_handle << "P3\n" << width << ' ' << height << "\n255\n";
-  
-  std::vector<double> temps;  
+
+  std::vector<double> temps;
   auto const time_start_render = std::chrono::steady_clock::now();
   for (int j = height - 1; j >= 0; --j) {
     for (int i = 0; i < width; ++i) {
@@ -74,19 +78,19 @@ void render_particles(std::vector<Particle*> const& particles, QuadTree const& q
       double y = pix2pic(j, height);
 
       double const temp = [&]() {
-                            V2 const loc{x, y};
-                            if (use_quadtree) {
-                              return quadtree.get_temperature(loc);
-                            } else {
-                              double temp = 0.;
-      for (Particle* const p : particles) {
-        temp += temperature_contrib(loc, *p);
-      }
-      return temp;
-                            }
-                          }();
+        V2 const loc{x, y};
+        if (use_quadtree) {
+          return quadtree.get_temperature(loc);
+        } else {
+          double temp = 0.;
+          for (Particle *const p : particles) {
+            temp += temperature_contrib(loc, *p);
+          }
+          return temp;
+        }
+      }();
       temps.push_back(temp);
-      
+
       // These vars scale from 0 to 1.
       auto r = std::tanh(temp / 100.);
       auto g = std::tanh(temp / 100000.);
@@ -102,28 +106,31 @@ void render_particles(std::vector<Particle*> const& particles, QuadTree const& q
   }
   file_handle.close();
   auto const time_end_render = std::chrono::steady_clock::now();
-  std::chrono::duration<double> const diff = time_end_render - time_start_render;
-  std::cout << "time to render: " << diff.count() << '\n';  
+  std::chrono::duration<double> const diff =
+      time_end_render - time_start_render;
+  std::cout << "time to render: " << diff.count() << '\n';
   std::cout << "max temp: " << *std::max_element(temps.cbegin(), temps.cend())
             << '\n';
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   // CLI flags
   bool use_quadtree = true;
   if (argc > 1) {
     char const opt = *argv[1];
     std::cout << opt << '\n';
-    if (opt == 'q') use_quadtree = true;
-    else if (opt == 'n') use_quadtree = false;
+    if (opt == 'q')
+      use_quadtree = true;
+    else if (opt == 'n')
+      use_quadtree = false;
   }
-  
+
   constexpr int WIDTH = 255 * 2;
   constexpr int HEIGHT = 255 * 2;
-  
+
   // Initial conditions
   std::vector<V2> positions = genInitialPositions();
-  std::vector<Particle*> particles;
+  std::vector<Particle *> particles;
   std::transform(positions.cbegin(), positions.cend(),
                  std::back_inserter(particles), [](const V2 &position) {
                    return new Particle{
@@ -140,5 +147,3 @@ int main(int argc, char* argv[]) {
 
   render_particles(particles, quadtree, WIDTH, HEIGHT, 0, use_quadtree);
 }
-
-
